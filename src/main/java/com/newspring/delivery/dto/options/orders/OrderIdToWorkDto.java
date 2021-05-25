437147c0151ddbbdd8e2b66a662ff7d97b0c1d1e
Zhulikov_Dmitriy <titan_22@mail.ru>
2021-05-25 15:18:02 +0300

    ветка authentication 25,05,2021

---
+++ src/main/java/com/newspring/delivery/dto/options/orders/OrderIdToWorkDto.java
@@ -0,0 +1,12 @@
+package com.newspring.delivery.dto.options.orders;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderIdToWorkDto {
+    private Long orderId;
+}
